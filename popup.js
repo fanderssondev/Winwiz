ce5ebed5360ca6952eceeb42b00c1313d39fc89a
nanobyter <sotarfredrik@gmail.com>
2022-12-11 18:32:57 +0100

    Enabled searching for VIN in the correct URL

--- popup.js
+++ popup.js
@@ -2,11 +2,6 @@ const brand = document.getElementById("brand");
 const vinform = document.getElementById("vin-form");
 const vinNumber = document.getElementById("vin-number");
 
-// Get the Brand
-// brand.addEventListener("change", (event) => {
-//   event.preventDefault();
-//   console.log(brand.value);
-// });
 
 // Get the VIN number
 vinform.addEventListener("submit", (event) => {
@@ -15,7 +10,28 @@ vinform.addEventListener("submit", (event) => {
   searchVIN(brand, vinNumber);
 });
 
+// Search the site for the VIN
 function searchVIN(brand, vinNumber) {
-  console.log("searchVIN");
-  window.open(`https://www.bike-parts-honda.es/`, "_blank");
+  const URL = getURL(brand, vinNumber);
+  window.open(URL, "_blank");
+}
+
+// Construct the URL
+function getURL(brand, vinNumber) {
+  let url = "";
+  switch (brand.value) {
+    case "honda":
+      url =
+        `https://www.bike-parts-honda.com/honda-motorcycle/search_vehicle/chassis/${vinNumber.value}+`;
+      break;
+    case "yamaha":
+      url =
+        `https://www.bike-parts-yam.com/yamaha-motorcycle/search_vehicle/chassis/${vinNumber.value}+`;
+      break;
+
+    default:
+      break;
+  }
+  console.log(url);
+  return url;
 }

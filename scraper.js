32a8d7dfaf86acc4685f4f81ed8676a042f3ddd5
nanobyter <sotarfredrik@gmail.com>
2023-01-01 20:58:43 +0100

    Updated styling

--- scraper.js
+++ scraper.js
@@ -2,10 +2,21 @@ const cherio = require("cherio");
 const request = require("request");
 
 request(
-    "https://www.bike-parts-honda.es/honda-motocicleta/busqueda_vehiculo/chasis/ZDCPC41F0BF102475+",
-    (error, response, html) => {
-      if (!error && response.statusCode === 200) {
-        console.log(html);
-      }
+  "https://www.bike-parts-honda.es/honda-motocicleta/busqueda_vehiculo/chasis/ZDCPC41F0BF102475+",
+  (error, response, html) => {
+    if (!error && response.statusCode === 200) {
+      const $ = cherio.load(html);
+
+      //   const card = $(".js_filter_card div div div a");
+      //   console.log(card.text());
+
+      $(".js_filter_card div div div a").each((i, el) => {
+        const item = $(el).text();
+        console.log(item);
+        console.log(
+          "************************************************************************************************"
+        );
+      });
     }
-  );
+  }
+);
